7ae7bbef874a94df4d275f9a24623485c69d0771
Scott Goldstein <goldstein.scott@aol.com>
2015-06-03 21:23:01 -0400

    Class End June 3

--- Class Examples/Practice-Examples-3/Lesson5.js
+++ Class Examples/Practice-Examples-3/Lesson5.js
@@ -1,15 +1,29 @@
 (function(app){
+	
+	app.config(function(SECRET_SERVER_CODE){
 
+	});
 
 	app.controller("myCtrl", function(THEMES){
 		var self = this;
 		self.themes = THEMES;
 	});
 
-	app.controller("myCtrl2", function(THEMES, COLOR){
+	app.controller("myCtrl2", function(THEMES, SECRET_SERVER_CODE, AgeCalculateService){
 		var self = this;
 		self.themes = THEMES;
-		self.favoriteColor = COLOR;
+		self.secretCode = SECRET_SERVER_CODE;
+
+		self.cars = AgeCalculateService.cars;
+	});
+
+	app.controller("myCtrl3", function(CarDTO, AgeCalculateService){
+		var self = this;
+		self.myCar = new CarDTO("Honda", "Accord", 1960);
+		self.myCar2 = new CarDTO("Subaru", "Impreza", 2014);
+		self.standardCar = CarDTO.buildMyStandardCar();
+
+		AgeCalculateService.cars.push(self.myCar, self.myCar2, self.standardCar);
 	});
 
 })(angular.module("lesson5App", ["lesson5AppService"]))
--- Class Examples/Practice-Examples-3/Lesson5Services.js
+++ Class Examples/Practice-Examples-3/Lesson5Services.js
@@ -12,6 +12,42 @@
 		}
 	});
 
-	app.constant("COLOR", "Blue is Scotts Favorite Color!");
+	app.constant("SECRET_SERVER_CODE", "123456789");
+
+	app.factory("CarDTO", function(AgeCalculateService){
+
+		var serialNumber = "123456789";
+
+		function Car(make, model, year){
+			this.make = make;
+			this.model = model;
+			this.year = year;
+			this.age = AgeCalculateService.calcAge(year);
+		}
+
+		Car.prototype.updateAge = function(){
+			this.age = AgeCalculateService.calcAge(this.year);
+		};
+
+		Car.prototype.getSerialNumber = function(){
+			return serialNumber;
+		};
+
+		Car.buildMyStandardCar = function() {
+			return new Car("Ford","Mustang", 1990);
+		};
+
+		return Car;
+	});
+
+	app.service("AgeCalculateService", function(){
+		var self = this;
+
+		self.calcAge = function(year){
+			return 2015 - year;
+		};
+
+		self.cars = [];
+	});
 
 })(angular.module("lesson5AppService", []))
