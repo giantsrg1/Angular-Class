18aec5a2d69204ba377ffea4719f30803f73e045
Scott Goldstein <goldstein.scott@aol.com>
2015-06-17 19:38:48 -0400

    Adding Template and Lesson9

---
+++ Class Examples/Lesson9/src/app/main/main.controller.js
@@ -0,0 +1,8 @@
+"use strict"
+
+(function(app){
+
+
+
+
+})(aungular.module("Lesson9"))
---
+++ gulpTemplate/src/app/components/navbar/navbar.controller.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('lessonTemplate')
+  .controller('NavbarCtrl', function ($scope) {
+    $scope.date = new Date();
+  });
